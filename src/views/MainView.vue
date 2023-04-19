2b652dc64300716181045eccd18740ee9db2d09d
genie <geniejinhee7@gmail.com>
2023-04-19 15:04:20 +0900

    test

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="main">
     <img src="../assets/image/모행여행로고.png">
-    <div><button @click="login()">Login</button></div>
-    <div><button @click="signup()">Sign Up!!!  </button></div>
+    <div><button @click="login()">Login</buttongit></div>
+    <div><button @click="signup()">Sign Up!!!</button></div>
   </div>
 </template>
 gi
