931fd5c1b8273114a036cf122b6a162d47ccffcb
jaejinlee1008 <ghkfuddks@naver.com>
2023-05-19 18:55:56 +0900

    23/05/19_재진 마이 코스 제목 클릭 시 내용 보이게

--- src/components/MyPageCourseBody.vue
+++ src/components/MyPageCourseBody.vue
@@ -6,11 +6,7 @@
       class="elevation-1"
     >
       <template v-slot:[`item.courseTitle`]="{ item }">
-        <v-btn 
-        text 
-        color="primary"
-        :disabled="item.travelStatus == 'N'"
-        >
+        <v-btn text color="primary" @click="openContent(item)">
           {{ item.courseTitle }}
         </v-btn>
       </template>
@@ -30,13 +26,59 @@
       <v-card>
         <v-card-title class="headline">방문 여부 확인</v-card-title>
         <v-card-text class="text-center">
-          추천드린 코스는 즐거우셨나요?<br>
-          확인 버튼을 누르시면 방문 상태가 변경됩니다.<br>
+          추천드린 코스는 즐거우셨나요?<br />
+          확인 버튼을 누르시면 방문 상태가 변경됩니다.<br />
           만약 아직 코스를 방문하지 않았다면 취소를 눌러주세요.
         </v-card-text>
         <v-card-actions class="justify-center">
-          <v-btn x-large color="primary" @click="confirmTravelStatus" class="mr-4">변경</v-btn>
+          <v-btn
+            x-large
+            color="primary"
+            @click="confirmTravelStatus"
+            class="mr-4"
+            >변경</v-btn
+          >
           <v-btn x-large color="error" @click="cancelModal">취소</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-dialog v-model="showContent" max-width="600px">
+      <v-card>
+        <v-card-title class="headline">여행 코스</v-card-title>
+        <v-card-text class="text-center">
+          <v-timeline>
+            <v-timeline-item
+              v-for="(values, key) in $store.getters.getCourseMap"
+              :key="key"
+              color="blue"
+              small
+            >
+              <!-- <template v-slot:opposite>
+            <span
+              :class="`headline font-weight-bold blue--text`"
+              v-text="key"
+            ></span>
+          </template> -->
+              <v-card>
+                <v-card-title class="blue lighten-2">
+                  <h2
+                    class="text-h4 white--text font-weight-light"
+                    v-text="key"
+                  ></h2>
+                </v-card-title>
+                <v-card-text>
+                  <ul class="items">
+                    <li v-for="(answer, i) in values" :key="i">
+                      {{ answer }}
+                    </li>
+                  </ul>
+                </v-card-text>
+              </v-card>
+            </v-timeline-item>
+          </v-timeline>
+        </v-card-text>
+        <v-card-actions class="justify-center">
+          <v-btn x-large color="error" @click="cancelModal">닫기</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -60,7 +102,8 @@ export default {
         { text: "방문여부", value: "travelStatus" },
       ],
       modalOpen: false,
-      selectedItem: null
+      showContent: false,
+      selectedItem: null,
     };
   },
   methods: {
@@ -68,11 +111,30 @@ export default {
       this.selectedItem = item;
       this.modalOpen = true;
     },
+    openContent(item) {
+      this.selectedItem = item;
+      this.showContent = true;
+      axios
+        .get(this._baseUrl + "course/schedule", {
+          params: {
+            answer: this.selectedItem.courseContents,
+          },
+        })
+        .then((result) => {
+          console.log(result);
+          this.$store.commit("setSchedule", result.data);
+          this.$store.commit("setCourseMap", result.data.tripmap);
+          // this.$store.commit("setIsLoading", false);
+        })
+        .catch(function () {
+          console.log("fail");
+        });
+    },
     confirmTravelStatus() {
       console.log(this.selectedItem.courseNum);
       axios
         .put(this._baseUrl + "courseBoard/updateCourse", {
-          courseNum : this.selectedItem.courseNum
+          courseNum: this.selectedItem.courseNum,
         })
         .then((result) => {
           if (result.data == 1) {
@@ -93,7 +155,7 @@ export default {
                   "setMyComments",
                   JSON.parse(result2.data.commentlist)
                 );
-                this.$store.commit("setCount",result2.data.count);
+                this.$store.commit("setCount", result2.data.count);
               })
               .catch(function (error) {
                 console.error("에러 ", error);
@@ -109,7 +171,8 @@ export default {
     cancelModal() {
       // Close the modal without changing the travelStatus
       this.modalOpen = false;
-    }
+      this.showContent = false;
+    },
   },
 };
 </script>
