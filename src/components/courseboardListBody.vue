5c7c245e549b3aea7fa1cd2a11623f6a49630c48
genie <geniejinhee7@gmail.com>
2023-05-23 18:08:35 +0900

    23/05/23_지니 코스목록 이미지수정

--- src/components/courseboardListBody.vue
+++ src/components/courseboardListBody.vue
@@ -1,63 +1,60 @@
 <template>
-<div>
-  <hr/>
-  <v-container fluid>
-     <v-row>
-       <v-col cols="9">
-         <v-row justify="center">
-           <v-col cols="12" md="10">
-             <v-card class="mb-5">
-               <v-card-title class="text-center title font-weight-bold custom-card-title">코스 목록</v-card-title>
-               <v-card-text>
-                 <v-list>
-                   <v-list-item
-                     v-for="(course, index) in paginatedCourses"
-                     :key="index"
-                     @click="goBoard(index, course.courseNum)"
-                     class="py-2"
-                     two-line
-                   >
-                     <v-list-item-content>
-                       <v-list-item-title class="subtitle-1" style="color: #00000099; font-weight:900; font-size: 1.2em;">{{ course.courseTitle }}</v-list-item-title>
-                       <v-list-item-subtitle class="caption" style="font-weight:800">{{ course.courseLike }} Likes</v-list-item-subtitle>
-                     </v-list-item-content>
-                   </v-list-item>
-                 </v-list>
-                 <v-pagination v-model="page" :length="totalPages" />
-               </v-card-text>
-             </v-card>
-           </v-col>
-         </v-row>
-       </v-col>
+  <div>
+    <img src="../assets/image/메타세쿼이아길.png" alt="courselist" style="width: 100%;" />
 
+    <v-container fluid>
+      <v-row>
+        <v-col cols="9">
+          <v-row justify="center">
+            <v-col cols="12" md="10">
+              <v-card class="mb-5" outlined tile>
+                <v-card-title class="text-center title font-weight-bold custom-card-title">코스 목록</v-card-title>
+                <v-card-text>
+                  <v-list>
+                    <v-list-item
+                      v-for="(course, index) in paginatedCourses"
+                      :key="index"
+                      @click="goBoard(index, course.courseNum)"
+                      class="py-2"
+                      two-line
+                    >
+                      <v-list-item-content>
+                        <v-list-item-title class="subtitle-1" style="color: #00000099; font-weight: 900; font-size: 1.2em;">{{ course.courseTitle }}</v-list-item-title>
+                        <v-list-item-subtitle class="caption text-right" style="font-weight: 800">{{ course.courseLike }} Likes</v-list-item-subtitle>
+                      </v-list-item-content>
+                    </v-list-item>
+                  </v-list>
+                  <v-pagination v-model="page" :length="totalPages" />
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-col>
 
-      <v-col cols="3">
-        <v-card class="elevation-2 mb-5 best-course-card" outlined tile>
-          <v-card-title class="text-center title font-weight-bold" style="background-color: #f8f9fa;">베스트 순위</v-card-title>
-          <v-list>
-            <v-list-item
-              v-for="(bestCourse, index) in topTenCourses"
-              :key="index"
-              @click="goLikeBoard(index, bestCourse.courseNum)"
-              class="py-2"
-              two-line
-              link
-            >
-              <v-list-item-content>
-                <v-list-item-title class="subtitle-1 font-weight-bold" style="color: #00000099;">{{ bestCourse.courseTitle }}</v-list-item-title>
-                <v-list-item-subtitle class="caption">{{ bestCourse.courseLike }} Likes</v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
-</div>
+        <v-col cols="3">
+          <v-card class="elevation-2 mb-5 best-course-card" outlined tile>
+            <v-card-title class="text-center title font-weight-bold custom-card-title">베스트 순위</v-card-title>
+            <v-list>
+              <v-list-item
+                v-for="(bestCourse, index) in topTenCourses"
+                :key="index"
+                @click="goLikeBoard(index, bestCourse.courseNum)"
+                class="py-2"
+                two-line
+                link
+              >
+                <v-list-item-content>
+                  <v-list-item-title class="subtitle-1 font-weight-bold" style="color: #00000099;">{{ bestCourse.courseTitle }}</v-list-item-title>
+                  <!-- <v-list-item-subtitle class="caption">{{ bestCourse.courseLike }} Likes</v-list-item-subtitle> -->
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
-
-
-
 
 <script>
 import axios from "axios";
@@ -77,7 +74,7 @@ export default {
   },
   computed: {
   topTenCourses() {
-    return this.sortedCourseList.slice(0, 10);
+    return this.sortedCourseList.slice(0, 5);
   },
   totalPages() {
       return Math.ceil(this.$store.getters.getCourseList.length / this.itemsPerPage);
@@ -195,4 +192,25 @@ export default {
   font-size: 2em !important;
 }
 
+/* 이미지 스타일 */
+img {
+  display: block;
+  margin: 0 auto;
+  max-width: 100%;
+  height: auto;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+/* 코스 목록 카드 스타일 */
+.v-card {
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+/* 베스트 순위 카드 스타일 */
+.best-course-card {
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
 </style>
