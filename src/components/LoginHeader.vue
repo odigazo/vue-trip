9b28f38a0ee01b09bb611214a51ba9de1f402450
rlatks1217 <rlatks1217@gmail.com>
2023-06-03 16:49:34 +0900

    로그인 회원가입 버튼 css

--- src/components/LoginHeader.vue
+++ src/components/LoginHeader.vue
@@ -1,49 +1,49 @@
 <template>
   <div class="loginheader">
-    <img class="logo" src="../assets/image/triplogo.png"/>
-    <div class="middle">로그인 페이지</div>
-    <div class="signup"><a @click="signup()">회원가입</a></div>
+    <img class="logo" src="../assets/image/triplogo.png" />
+    <div class="middle"></div>
+    <v-btn text color="primary" style="font: inherit" x-large @click="signup()"
+      >회원가입</v-btn
+    >
   </div>
 </template>
 
 <script>
-
-export default({
-  methods:{
-    signup(){
-      this.$router.push({name:"signup"})
-    }
-  }
-})
+export default {
+  methods: {
+    signup() {
+      this.$router.push({ name: "signup" });
+    },
+  },
+};
 </script>
 
 
 <style scoped>
-  div.loginheader {
-    display: flex;
-    align-items: center;
-    height: 80px;
-    background-color: rgb(255, 255, 255);
-  }
+div.loginheader {
+  display: flex;
+  align-items: center;
+  height: 80px;
+  background-color: rgb(255, 255, 255);
+}
 
-  div.middle {
-    flex: 1;
-    text-align: center;
-    font-size: 24px; /* 글자 크기 */
-    font-weight: bold; /* 글자 굵기 */
-  }
+div.middle {
+  flex: 1;
+  text-align: center;
+  font-size: 24px; /* 글자 크기 */
+  font-weight: bold; /* 글자 굵기 */
+}
 
-  div.signup {
-    text-align:right;
-    color: blue;
-    text-decoration: underline;
-    cursor: pointer;
-  }
+div.signup {
+  text-align: right;
+  color: blue;
+  text-decoration: underline;
+  cursor: pointer;
+}
 
-   .logo {
-    width: 160px;
-    border-radius: 50%;
-    margin-left: 20px;
-  }
-
+.logo {
+  width: 160px;
+  border-radius: 50%;
+  margin-left: 20px;
+}
 </style>
